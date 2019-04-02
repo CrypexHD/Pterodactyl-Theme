744f66237d31160c6ce1486765716d37b5406657
CrypexHD <aksel.srensen@gmail.com>
2019-04-02 11:53:59 +0200

    Add files via upload

--- MasterThemes/BlackEndSpace/build.sh
+++ MasterThemes/BlackEndSpace/build.sh
@@ -21,7 +21,7 @@ if [ ! -f "artisan" ]; then
     zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
 
     echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/TheFonix/Pterodactyl-Themes.git .
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/CrypexHD/Pterodactyl-Theme.git .
     cp -r MasterThemes/BlackEndSpace/public ..
     cp -r MasterThemes/BlackEndSpace/resources ..
 
