d58bcb7fda35e57d7497e28d64758d7468922aff
CrypexHD <aksel.srensen@gmail.com>
2019-04-09 12:14:01 +0200

    Add files via upload

--- MasterThemes/BlackEndSpace/resources/themes/pterodactyl/auth/login.blade.php
+++ MasterThemes/BlackEndSpace/resources/themes/pterodactyl/auth/login.blade.php
@@ -50,7 +50,8 @@
             </div>
             <div class="row">
                 <div class="col-xs-4">
-                    <a href="{{ route('auth.password') }}"><button type="button" class="btn pterodactyl-login-button--left"><i class="fa fa-life-ring"></i></button></a>
+                    <a href="{{ route('auth.password') }}"><button type="button" class="btn pterodactyl-login-button--left"><i class="fa fa-cog fa-spin fa-3x fa-fw"></i>
+<span class="sr-only">Loading...</span></button></a>
                 </div>
                 <div class="col-xs-offset-4 col-xs-4">
                     {!! csrf_field() !!}
