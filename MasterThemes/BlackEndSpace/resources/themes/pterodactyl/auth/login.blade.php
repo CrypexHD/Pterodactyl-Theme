be5d0fd33e5e37f42cbc4afb5b4285038c8a0ae4
CrypexHD <aksel.srensen@gmail.com>
2019-04-09 12:23:06 +0200

    Add files via upload

--- MasterThemes/BlackEndSpace/resources/themes/pterodactyl/auth/login.blade.php
+++ MasterThemes/BlackEndSpace/resources/themes/pterodactyl/auth/login.blade.php
@@ -50,6 +50,7 @@
             </div>
             <div class="row">
                 <div class="col-xs-4">
+                    <br />
                     <a href="{{ route('auth.password') }}"><p>Struggling to login?</p></a>
                 </div>
                 <div class="col-xs-offset-4 col-xs-4">
